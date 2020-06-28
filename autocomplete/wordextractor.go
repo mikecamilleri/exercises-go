7530a9e87042520db701af28ec3bbf732497906b
Michael Camilleri <mike@mikecamilleri.com>
2020-06-28 15:46:50 -0400

    update comments for wordextractor

--- autocomplete/wordextractor.go
+++ autocomplete/wordextractor.go
@@ -7,7 +7,12 @@ import (
 	"unicode"
 )
 
-// ExtractWordsFromFile ...
+// ExtractWordsFromFile extracts words from a text file.
+//
+// Words are emitted on wordChan. A wrod is defined as a string of characters
+// containing only letters, hyphens, and apostrophies. Words may not begin or
+// end with a non-letter character and may not have two of the same non-letter
+// characters in a row.
 func ExtractWordsFromFile(path string, wordChan chan<- string) {
 	// defer letting caller know when we are done by closing the channel
 	defer close(wordChan)
@@ -15,14 +20,13 @@ func ExtractWordsFromFile(path string, wordChan chan<- string) {
 	// open the file and defer close
 	file, err := os.Open(path)
 	if err != nil {
-		// improve error handling
-		return
+		return // TODO: improve error handling
 	}
 	defer file.Close()
 
-	// create the scanner
+	// create a scanner for the file
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanWords)
+	scanner.Split(bufio.ScanWords) // scan for words
 
 	// scan
 	for scanner.Scan() {
@@ -43,8 +47,7 @@ func ExtractWordsFromFile(path string, wordChan chan<- string) {
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		// improve error handling
-		return
+		return // TODO: improve error handling
 	}
 
 	return
@@ -52,7 +55,7 @@ func ExtractWordsFromFile(path string, wordChan chan<- string) {
 
 // cleanWord removes non-alphabet characters from the beginning and end of the
 // word and ensures that the word only contains alphabet letters, hyphens, and
-// apostrophies internally.
+// apostrophies internally; and not two in a row.
 func cleanWord(word string) string {
 	// remove unacceptable characters from word
 	var stageOneWord strings.Builder
@@ -62,7 +65,7 @@ func cleanWord(word string) string {
 		}
 	}
 
-	// strip characters excepted in stage one from left and right
+	// trim characters excepted in stage one from left and right
 	stageTwoWord := strings.TrimRight(strings.TrimLeft(stageOneWord.String(), "-'’"), "-'’")
 
 	// if the word contains any repeated special characters internally, it is
