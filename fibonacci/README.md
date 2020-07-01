2cdd22f32bb0dd8f0ab4c3cd2cb5afe9a647dd28
Michael Camilleri <mike@mikecamilleri.com>
2020-07-01 13:07:13 -0400

    add fibonacci implementations  to repo

---
+++ fibonacci/README.md
@@ -0,0 +1,3 @@
+# Fibonacci!
+
+The Fibonacci sequence is commonly used to illustrate recursion and as an exercise to test basic programming skills in any language. 
---
+++ fibonacci/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+// f(0) == 0
+// f(1) == 1
+// f(n) == f(n-1) + f(n-2)
+
+// FibR recursively calculates and returns the nth Fibonacci number.
+// O(2^n)
+func fibR(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibR(n-1) + fibR(n-2)
+}
+
+// FibI iteratively calculates and returns the nth Fibonacci number.
+// O(n)
+func fibI(n int) int {
+	ppn := 0 // previous previous number
+	pn := 0  // previous number
+	cn := 1  // current number
+	if n == 0 {
+		return 0
+	}
+	for i := 1; i < n; i++ {
+		ppn = pn
+		pn = cn
+		cn = ppn + pn
+	}
+	return cn
+}
+
+func main() {
+	fmt.Printf("Fib(n) = recursive = iterative\n")
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("Fib(%d) = %d = %d\n", i, fibR(i), fibI(i))
+	}
+}
